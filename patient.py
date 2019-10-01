ed0442a169e4a0a0a6045dd4bfd6d02e61955718
rhettdsouza13 <rhettdsouza13@gmail.com>
2019-10-01 04:36:30 -0500

    Adding Additional Functions

--- patient.py
+++ patient.py
@@ -3,17 +3,20 @@ class Patient:
     def __init__(self):
         pass
 
-    def process_start_info(text_block,fields):
+    def process_start_info(self,text_block,fields):
         pass
 
-    def process_pat_info(text_block,fields):
+    def process_pat_info(self,text_block,fields):
         pass
 
-    def process_encounter_info(text_block,fields):
+    def process_encounter_info(self,text_block,fields):
         pass
 
-    def process_guaranter_info(text_block,fields):
+    def process_guaranter_info(self,text_block,fields):
         pass
 
-    def process_coverage_info(text_block,fields):
+    def process_coverage_info(self,text_block,fields):
+        pass
+
+    def csv_rep(self):
         pass
