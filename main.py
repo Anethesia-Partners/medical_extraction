08dfba262d525b0eaa20cac1e87d2bfacc3c006d
rhettdsouza13 <rhettdsouza13@gmail.com>
2019-10-02 04:45:19 -0500

    Added reading in of coloned fields

--- main.py
+++ main.py
@@ -10,20 +10,44 @@ from pdf2image import convert_from_path, convert_from_bytes
 from request_handling import get_text
 from patient import Patient
 import re
+import pandas as pd
 
-def get_blocks(text_block, block_markers,breaking_phrase):
+
+
+def get_patients(text_block, block_markers,breaking_phrase):
+
+    patient_list = []
+
     text_block = ['<START>'] + text_block
     blocks = {x:[] for x in block_markers}
     curr_marker = ''
+    curr_patient = Patient()
+
     for line in text_block:
+
         if breaking_phrase in line.strip():
+            curr_patient.process_gen_info(blocks)
+            patient_list.append(curr_patient)
+            print (curr_patient.pat_dic)
             curr_marker = '<START>'
-            print("NEW")
+            blocks = {x:[] for x in block_markers}
+            curr_patient = Patient()
+
         if line.strip() in block_markers:
                 curr_marker = line.strip()
                 continue
+
         blocks[curr_marker].append(line)
-    return blocks
+    curr_patient.process_gen_info(blocks)
+    print(curr_patient.pat_dic,blocks)
+    return patient_list
+
+def compile_dataframe(patient_list):
+    pat_df = pd.DataFrame()
+    for patient in patient_list:
+        pat_df = pd.concat([pat_df,pd.DataFrame([patient.csv_rep()], columns=patient.csv_rep().keys())],axis=0,join='outer').reset_index()
+        print(pat_df)
+    return pat_df
 
 full_body = get_text('gs://report-ap/test_image.jpg').full_text_annotation.text.splitlines()
 record = []
@@ -31,4 +55,5 @@ pat_fl = 0
 nam_nl = 0
 block_markers = ['<START>', 'ENCOUNTER', 'PATIENT', 'GUARANTOR', 'COVERAGE']
 breaking_phrase = 'QUAD CITIES'
-print(get_blocks(full_body,block_markers,breaking_phrase))
+patient_list = get_patients(full_body,block_markers,breaking_phrase)
+print(compile_dataframe(patient_list))
--- patient.py
+++ patient.py
@@ -1,22 +1,41 @@
 
 class Patient:
     def __init__(self):
+        self.pat_dic = {}
+        self.fields = {'Encounter Date', 'Hospital Account', 'MRN', 'Guarantor', 'Contact Serial #', 'Name', 'Address','City'}
+    # def process_block(self, text_block, fields):
+    #     self.process_pat_info(text_block['PATIENT'], fields)
+    #     self.process_start_info(text_block['<START>'], fields)
+    #     self.process_encounter_info(text_block['ENCOUNTER'], fields)
+    #     self.process_guaranter_info(text_block['GUARANTOR'], fields)
+    #     self.process_coverage_info(text_block['COVERAGE'], fields)
+
+
+    def process_gen_info(self,text_block):
+        for key, value in text_block.items():
+            for line in value:
+                if any(field == line.split(':')[0] for field in self.fields) and len(line.split(':'))>1:
+                    print (line)
+                    if key != '<START>':
+                        self.pat_dic[key+ '_' + line.split(':')[0].strip()] = line.split(':')[1].strip()
+                    else:
+                        self.pat_dic[line.split(':')[0].strip()] = line.split(':')[1].strip()
+
+    def process_start_info(self, text_block, fields):
         pass
 
-    def process_start_info(self,text_block,fields):
+
+    def process_pat_info(self, text_block, fields):
         pass
 
-    def process_pat_info(self,text_block,fields):
+    def process_encounter_info(self, text_block, fields):
         pass
 
-    def process_encounter_info(self,text_block,fields):
+    def process_guaranter_info(self, text_block, fields):
         pass
 
-    def process_guaranter_info(self,text_block,fields):
-        pass
-
-    def process_coverage_info(self,text_block,fields):
+    def process_coverage_info(self, text_block, fields):
         pass
 
     def csv_rep(self):
-        pass
+        return self.pat_dic
