29ca3b8ec8aa04e5d539edd6597c1eb52e1aa1f5
rhettdsouza13 <rhettdsouza13@gmail.com>
2019-10-10 11:41:38 -0500

    Structuring update

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ def get_patients(text_block, block_markers,breaking_phrase):
         if breaking_phrase in line.strip():
             curr_patient.process_gen_info(blocks)
             patient_list.append(curr_patient)
-            print (curr_patient.pat_dic)
             curr_marker = '<START>'
             blocks = {x:[] for x in block_markers}
             curr_patient = Patient(block_markers=block_markers)
@@ -39,19 +38,17 @@ def get_patients(text_block, block_markers,breaking_phrase):
 
         blocks[curr_marker].append(line)
     curr_patient.process_gen_info(blocks)
-    print(curr_patient.pat_dic,blocks)
     return patient_list
 
 def compile_dataframe(patient_list):
     pat_df = pd.DataFrame()
     for patient in patient_list:
         pat_df = pd.concat([pat_df,pd.DataFrame([patient.csv_rep()], columns=patient.csv_rep().keys())],axis=0,join='outer').reset_index(drop=True)
-
-        print(pat_df)
     pat_df = pat_df.dropna(axis=1, how='all')
     return pat_df
 
 full_body = get_text('gs://report-ap/test_image.jpg').full_text_annotation.text.splitlines()
+
 record = []
 pat_fl = 0
 nam_nl = 0
--- patient.py
+++ patient.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 class Patient:
     def __init__(self,block_markers):
@@ -7,43 +8,54 @@ class Patient:
                     'Status','Home Phone','Patient Class','Hospital Service','Unit','Bed','Admitting Physician','Attending Physician',
                     'Adm Diagnosis', 'Coded Procedure'}
         self.pat_dic = {}
+        self.update_keys(block_markers)
+
+
+    def process_gen_info(self,text_block):
+        for key, value in text_block.items():
+
+            if key == 'COVERAGE':
+                self.process_coverage_info(value)
+
+            self.process_coloned(value,key)
+
+    def process_coverage_info(self, text_block):
+        coverage_blocks = {'PRIMARY INSURANCE', 'SECONDARY INSURANCE'}
+        self.update_keys(coverage_blocks)
+        blocks = {x:[] for x in coverage_blocks}
+        curr_marker = ''
+
+        for line in text_block:
+            if line.strip() in coverage_blocks:
+                    curr_marker = line.strip()
+                    continue
+            elif curr_marker == '':
+                continue
+
+            blocks[curr_marker].append(line)
+        print(blocks)
+        for key,value in blocks.items():
+            self.process_coloned(value,key)
+
+
+    def process_coloned(self,text_block,key):
+        for line in text_block:
+            curr_line = line.strip().split(':')
+
+            if any(field == curr_line[0] for field in self.fields) and len(curr_line)>1 and '' not in curr_line:
+                if key != '<START>':
+                    self.pat_dic[key+ '_' + curr_line[0].strip()] = curr_line[1].strip()
+                else:
+                    self.pat_dic[curr_line[0].strip()] = curr_line[1].strip()
+
+    def update_keys(self,block_markers):
+
         for key in self.fields:
             for pref in block_markers:
                 if pref != '<START>':
                     self.pat_dic[pref+ '_' + key] = np.nan
                 else:
                     self.pat_dic[key] = np.nan
-
-
-    def process_gen_info(self,text_block):
-        for key, value in text_block.items():
-            for line in value:
-                curr_line = line.strip().split(':')
-
-                if key == 'COVERAGE':
-                    print (line)
-                if any(field == curr_line[0] for field in self.fields) and len(curr_line)>1 and '' not in curr_line:
-
-                    if key != '<START>':
-                        self.pat_dic[key+ '_' + curr_line[0].strip()] = curr_line[1].strip()
-                    else:
-                        self.pat_dic[curr_line[0].strip()] = curr_line[1].strip()
-
-    def process_start_info(self, text_block, fields):
-        pass
-
-
-    def process_pat_info(self, text_block, fields):
-        pass
-
-    def process_encounter_info(self, text_block, fields):
-        pass
-
-    def process_guaranter_info(self, text_block, fields):
-        pass
-
-    def process_coverage_info(self, text_block, fields):
-        pass
 
     def csv_rep(self):
         return self.pat_dic
